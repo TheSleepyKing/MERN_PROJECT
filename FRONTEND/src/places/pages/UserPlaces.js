20829b5d9708741ed204274926d5087d819b9196
Jameel Seboa <jameel.seboa@gmail.com>
2022-02-21 23:53:39 +0200

    added user specific paths and button styling

--- FRONTEND/src/places/pages/UserPlaces.js
+++ FRONTEND/src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from 'react-router-dom'
 
 import PlaceList from "../components/PlaceList";
 const DUMMY_PLACES =[
@@ -10,7 +11,7 @@ const DUMMY_PLACES =[
     address:"20 W 34th St, New York, NY 10001",
     location: {
         lat: 40.7484405,
-        lng: 73.9856644
+        lng: -73.9856644
     },
     creator: "u1",
     },
@@ -22,13 +23,15 @@ const DUMMY_PLACES =[
         address:"20 W 34th St, New York, NY 10001",
         location: {
             lat: 40.7484405,
-            lng: 73.9856644
+            lng: -73.9856644
         },
         creator:"u2",
         }
 ]
 const UserPlaces = () => {
-    return <PlaceList items={DUMMY_PLACES}/>
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces}/>
 };
 
 export default UserPlaces;
---
+++ FRONTEND/src/shared/components/UIElements/Modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Modal.css"
+
+const Modal = props => {
+
+};
+
+export default Modal;

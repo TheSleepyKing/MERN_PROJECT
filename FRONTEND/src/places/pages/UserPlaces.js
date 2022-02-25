d3d850dcaedfa43bbcd817d286424f650adace17
Jameel Seboa <jameel.seboa@gmail.com>
2022-02-25 08:52:59 +0200

    updated Modal.js

--- FRONTEND/src/places/pages/UserPlaces.js
+++ FRONTEND/src/places/pages/UserPlaces.js
@@ -1,37 +1,39 @@
 import React from "react";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
-const DUMMY_PLACES =[
-    {
+const DUMMY_PLACES = [
+  {
     id: "p1",
-    title:"Empire State Building",
-    description:"One of the most famous buildings in America",
-    imageUrl:"https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg",
-    address:"20 W 34th St, New York, NY 10001",
+    title: "Empire State Building",
+    description: "One of the most famous buildings in America",
+    imageUrl:
+      "https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg",
+    address: "20 W 34th St, New York, NY 10001",
     location: {
-        lat: 40.7484405,
-        lng: -73.9856644
+      lat: 40.7484405,
+      lng: -73.9856644,
     },
     creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous buildings in America",
+    imageUrl:
+      "https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
     },
-    {
-        id: "p2",
-        title:"Empire State Building",
-        description:"One of the most famous buildings in America",
-        imageUrl:"https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg",
-        address:"20 W 34th St, New York, NY 10001",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator:"u2",
-        }
-]
+    creator: "u2",
+  },
+];
 const UserPlaces = () => {
-    const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-    return <PlaceList items={loadedPlaces}/>
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
